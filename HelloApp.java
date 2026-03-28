b11e06ac925073caeb2ec02d625fdd0b68fb5593
Harry_Tamizhan <harryjaywilliam955@gmail.com>
2026-03-28 09:48:37 +0530

    Refactor greeting logic in HelloApp

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,18 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
-        } else {
+        if (args.length == 0) {
             System.out.println("Hello, World!");
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+            
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+            
+            if (nameBuilder.length() > 0) {
+                String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+                System.out.println("Hello, " + names + "!");
+            }
         }
     }
 }
