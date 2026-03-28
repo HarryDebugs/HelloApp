c9fba297b7906662808a658492e65b332057a677
Harry_Tamizhan <harryjaywilliam955@gmail.com>
2026-03-28 09:38:54 +0530

    Refactor HelloApp to use a message variable

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
+        String message;
+
         if (args.length > 0) {
-            System.out.println("Hello, " + args[0] + "!");
+            
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
         } else {
-            System.out.println("Hello, World!");
+            message = "Hello, World!";
         }
+
+        System.out.println(message);
     }
 }
